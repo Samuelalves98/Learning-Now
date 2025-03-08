3c472f13bfa50625a3d224617ee7fd26ae862e8a
Samuelalves98 <semeaos@gmail.com>
2025-03-07 23:38:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ var possuifaculdade = true
 console.log(nome,idade, possuifaculdade)
 
 var preco = 20;
-var totalComprado = 5;
+var totalComprado = 7;
 var precoTotal = preco * totalComprado;
 
 console.log(precoTotal)
+
+var sobrenome = 'Alves'
+var cidade = 'Campinas'
+
+console.log(sobrenome,cidade)
+
+var semdefinir
+
+console.log(semdefinir)
+
+var esposa = 'Iasmin'
