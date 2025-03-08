6e62a4843f430c09b9db8831e214bcbab5d9d997
Samuelalves98 <semeaos@gmail.com>
2025-03-07 23:05:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var nome = 'Samuel'
+var idade = 26
+var possuifaculdade = true
+
+console.log(nome,idade, possuifaculdade)
+
+var preco = 20;
+var totalComprado = 5;
+var precoTotal = preco * totalComprado;
+
+console.log(precoTotal)
